4bf1b1e6d0b7ef98c48017b610387214d43fc17c
Miguel Ramos <return.infox@gmail.com>
2023-01-13 19:07:02 -0300

    angular error 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { UserComponent } from './pages/user/user.component';
+import { UsersComponent } from './pages/users/users.component';
+import { Error404Component } from './pages/error404/error404.component';
+//* http://localhost:4200/
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'user/:id', component: UserComponent },
+  { path: 'users', component: UsersComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: '**', component: Error404Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
