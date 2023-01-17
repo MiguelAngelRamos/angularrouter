c049ace13783c6b6c4528d3d6a76963629de90b8
Miguel Ramos <return.infox@gmail.com>
2023-01-17 20:07:56 -0300

    app de router actualizar

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
-export class UserComponent {
+export class UserComponent implements OnInit{
+
+  constructor(
+    private dataService:DataService, 
+    private activatedRoute: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    this.getUserById();
+  }
+
+  getUserById() {
+    //* capturamos el parametro de la URL
+    this.activatedRoute.paramMap.subscribe( params => {
+      console.log(params);
+      let id = Number(params.get('id'));
+      // let id = (params.get('id'));
+      this.dataService.getUserById(id).subscribe(user => console.log(user));
+   
+    })
+  }
 
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IPost } from '../interfaces/IPost';
+import { IUser } from '../interfaces/IUser';
 
 
 @Injectable({
@@ -16,5 +17,12 @@ export class DataService {
   }
 
   //* manejar el error en caso de que falle el endpoint
+
+  getUserById(id: number): Observable<IUser> {
+    //* alt + 96 
+    return this.http.get<IUser>(`https://jsonplaceholder.typicode.com/users/${id}`);
+    // return this.http.get('https://jsonplaceholder.typicode.com/users/' + id);
+  }
+
 
 }
